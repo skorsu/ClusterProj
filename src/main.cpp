fa80477ead7fdcc0696d4ad8404cd9a7f70c3e7c
Kevin Korsurat <kevin.korsurat@colostate.edu>
2022-11-08 16:01:57 -0700

    update expand step
    - Not reset the cluster index.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,117 @@
 #include "RcppArmadillo.h"
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// User defined functions: -----------------------------------------------------
+// Note to self: ---------------------------------------------------------------
+// - Not reset the cluster index. (Maybe lower computational cost.)
+// - K is fixed. (We should include K as one of our input if necessary.)
+// - We should get the index of the existed cluster first in most step.
+// - For the contract step, still confused :(
 
+// Questions: ------------------------------------------------------------------
 // Questions (11/3)
-// - K is fixed? How about we start with K+ = K?
 // - Convert psi to alpha
-// - Reset the number of the clusters
-// - Step 1 fn return: new cluster, new alpha (psi)
+
+// User defined functions: -----------------------------------------------------
 
 // Step 1: Update the cluster space: -------------------------------------------
 // [[Rcpp::export]]
+Rcpp::List expand_cluster(int K, Rcpp::IntegerVector inactive_clus, 
+                          arma::uvec active_clus, arma::vec old_assign, 
+                          arma::vec psi, arma::vec xi, double a_theta, 
+                          double b_theta){
+  Rcpp::List result;
+  
+  /* Input: maximum cluster (K), inactive clusters, active clusters, 
+   *        previous cluster assignment, previous cluster weight (psi), 
+   *        hyperparameter for cluster (xi), hyperparameter (a_theta, b_theta)
+   * Output: new cluster weight, updated cluster assignment.
+   */
+  
+  // Select the cluster that we will expand
+  Rcpp::IntegerVector d_new_clus = Rcpp::sample(inactive_clus, 1);
+  int new_clus = d_new_clus[0];
+  
+  // Convert the cluster weight (psi) to the alpha.
+  // Since we assume that alpha_j ~ Gamma(xi_j, 1), alpha+ ~ Gamma(sum_xi, 1).
+  arma::vec xi_active = xi.rows(active_clus - 1);
+  double alpha_p = R::rgamma(sum(xi_active), 1);
+  arma::vec alpha_vec = alpha_p * psi;
+  double sum_active_alpha = sum(alpha_vec);
+  
+  // Sample alpha for new active cluster
+  alpha_vec.at(new_clus - 1) = R::rgamma(xi.at(new_clus - 1), 1);
+  
+  // Calculate the acceptance probability
+  arma::vec accept_prob = (alpha_vec.at(new_clus - 1)/alpha_vec) * 
+    (sum_active_alpha/(sum_active_alpha + alpha_vec.at(new_clus - 1))) * 
+    (a_theta/b_theta);
+  arma::vec A = arma::min(accept_prob, arma::ones(alpha_vec.size()));
+  
+  // Assign a new cluster
+  arma::vec new_assign = -1 * arma::ones(old_assign.size());
+  for(int i = 0; i < old_assign.size(); i++){
+    double u = arma::randu();
+    if(u <= A.at(old_assign.at(i) - 1)){
+      new_assign.at(i) = new_clus;
+    } else{
+      new_assign.at(i) = old_assign.at(i);
+    }
+  }
+  
+  // Adjust the psi vector
+  arma::vec new_alpha = arma::zeros(K);
+  arma::vec new_active_clus = arma::unique(new_assign);
+  arma::uvec index_active = arma::conv_to<arma::uvec>::from(new_active_clus) - 1;
+  new_alpha.elem(index_active) = alpha_vec.elem(index_active);
+  arma::vec new_phi = arma::normalise(new_alpha, 1);
+  
+  // Return the result
+  result["new_assign"] = new_assign;
+  result["new_phi"] = new_phi;
+
+  return result;
+}
+
+// [[Rcpp::export]]
+Rcpp::List expand_contract(int K, arma::vec old_assign, arma::vec psi,
+                           arma::vec xi, double a_theta, double b_theta){
+  Rcpp::List result;
+  
+  /* Input: maximum cluster (K), previous cluster assignment, 
+   *        previous cluster weight (psi), hyperparameter for cluster (xi),
+   *        hyperparameter (a_theta, b_theta).
+   * Output: new cluster weight, updated cluster assignment.
+   */ 
+  
+  // Indicate the existed clusters and inactive clusters
+  Rcpp::IntegerVector all_possible = Rcpp::seq(1, K);
+  Rcpp::IntegerVector existed_clus = Rcpp::as<Rcpp::IntegerVector>(Rcpp::wrap(arma::unique(old_assign)));
+  Rcpp::IntegerVector inactive_clus = Rcpp::setdiff(all_possible, existed_clus);
+  // result["y"] = inactive_clus;
+  
+  // Decide Expand or Contract. (1 = Expand; 0 = Contract)
+  int expand_indi;
+  int n_existed = existed_clus.size();
+  if(n_existed == 1){
+    expand_indi = 1;
+  } else if(n_existed == K){
+    expand_indi = 0;
+  } else {
+    expand_indi = R::rbinom(1, 0.5);
+  }
+  // result["ind"] = expand_indi;
+  
+  // MH algorithm
+  if(expand_indi == 1){
+    // Expand Function
+  } else {
+    // Contract Function
+  }
+  
+  return result;
+}
+
+/*
 Rcpp::List expand_cluster(arma::vec old_clus, arma::vec psi, arma::vec xi, 
                           double a_theta, double b_theta){
   Rcpp::List result;
@@ -72,6 +173,7 @@ Rcpp::List expand_cluster(arma::vec old_clus, arma::vec psi, arma::vec xi,
   result["accept_prob"] = accept_prob;
   return result;
 }
+*/
 
 // Step 2: Allocate the observation to the existing clusters: ------------------
 // Assume that all clusters share the same hyperparameters (gamma)
